3bd37ea749aded4ee748b73b6f77d8201f18c5e8
khushi-jAin09 <210929@sdmcujire.in>
2024-05-31 12:06:19 +0530

    first push

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h3></h3>
+<img src="https://st2.depositphotos.com/3591429/10464/i/450/depositphotos_104648666-stock-photo-group-of-people-brainstorming-on.jpg"/>
+    </div>
+  );
+}
+
+export default About;
